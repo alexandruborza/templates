dbae8f7735ce23264cff484f58aa0947a41faadd
dee-jay <dee_jay_mon@yahoo.com>
2015-12-10 12:18:26 +0100

    Create djrun.js

---
+++ html5/js/djrun.js
@@ -0,0 +1,12 @@
+/**
+ * Running javascript file
+ *
+ * @project		{project-name}
+ * @file      djrun.js
+ * @version		0.1.0
+ * @created		{now}
+ * @author		Alexandru Borza
+ * @email		  contact@alexandruborza.com
+ * @website		http://alexandruborza.net
+ * 
+ * */
