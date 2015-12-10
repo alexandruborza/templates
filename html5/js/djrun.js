f0077e79d14fd7c13c1cd6280a4c3469a8a1ec93
dee-jay <dee_jay_mon@yahoo.com>
2015-12-10 12:19:54 +0100

    Update djrun.js

--- html5/js/djrun.js
+++ html5/js/djrun.js
@@ -2,11 +2,11 @@
  * Running javascript file
  *
  * @project		{project-name}
- * @file      djrun.js
+ * @file		djrun.js
  * @version		0.1.0
  * @created		{now}
  * @author		Alexandru Borza
- * @email		  contact@alexandruborza.com
+ * @email		contact@alexandruborza.com
  * @website		http://alexandruborza.net
  * 
  * */
